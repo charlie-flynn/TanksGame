4646b52e82434f770e6b9138193ef16068689755
charlie-flynn <charlie.flynn.business@gmail.com>
2025-01-13 12:12:58 -0600

    created the shell of a components list
    amended it bvecause it broke EVERTYHTING somenhow

--- TanksGame/engine/Actor.h
+++ TanksGame/engine/Actor.h
@@ -1,17 +1,16 @@
 #pragma once
-#include "Vector2.h"
 #include "DynamicArray.h"
-
-typedef MathLibrary::Vector2 Vec2;
+#include "vector2.h"
 
 class Transform2D;
 class Component;
+
+typedef MathLibrary::Vector2 Vec2;
 
 class Actor 
 {
 public:
 	Actor();
-public:
 	~Actor();
 	static Actor* Instantiate(Actor* actor, Transform2D* transform, const Vec2 position, const float rotation);
 	void Destroy(Actor* actor);
@@ -31,18 +30,70 @@ public:
 	GetComponents
 	*/
 
+	template <typename T>
+	T* AddComponent(T component);
+	template <typename T>
+	T* AddComponent();
+	template <typename T>
+	bool RemoveComponent(T component);
+	template <typename T>
+	bool RemoveComponent();
+	template <typename T>
+	T* GetComponent();
+	template <typename T>
+	T* GetComponents();
+
 private:
 	Transform2D* m_transform;
+
 	bool m_started;
 	bool m_enabled;
+
+	DynamicArray<Component> m_components;
 
 	void virtual OnEnable() {};
 	void virtual OnDisable() {};
 	// make sure to have component lists when you inevitably make components
 
-	// other component functions to do later
+	// other component functions to do later (we might not need these ?)
 	/*
 	AddComponentToRemove
 	RemoveComponentsToBeRemoved
 	*/
 };
+
+template<typename T>
+inline T* Actor::AddComponent(T component)
+{
+	return nullptr;
+}
+
+template<typename T>
+inline T* Actor::AddComponent()
+{
+	return nullptr;
+}
+
+template<typename T>
+inline bool Actor::RemoveComponent(T component)
+{
+	return false;
+}
+
+template<typename T>
+inline bool Actor::RemoveComponent()
+{
+	return false;
+}
+
+template<typename T>
+inline T* Actor::GetComponent()
+{
+	return nullptr;
+}
+
+template<typename T>
+inline T* Actor::GetComponents()
+{
+	return nullptr;
+}

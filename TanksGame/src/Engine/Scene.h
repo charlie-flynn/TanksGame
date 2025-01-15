12cd8b7080dc0120a5f00a23251147eabb76117b
KevinHall2 <khjiitech@gmail.com>
2025-01-15 16:40:00 -0600

    added in functions in the Scene.cpp file
    includes the constructor, destructor, Start, Update, End, AddActor, and RemoveActor functions. Update, End, AddActor, and RemoveActor have had their functions filled out

--- TanksGame/src/Engine/Scene.h
+++ TanksGame/src/Engine/Scene.h
@@ -1,17 +1,20 @@
 #pragma once
 #include "Actor.h"
 #include "DynamicArray.h"
+#include <string>
 
 class Scene
 {
 
 private:
-	Scene(const DynamicArray<Actor>& _actors);
+
+	 DynamicArray<Actor>& _actors;
 
 public:
+	Scene();
 	~Scene();
 	void AddActor(Actor& actor);
-	bool RemoveActor(Actor& actor);
+	void RemoveActor(Actor& actor);
 	virtual void Start();
 	virtual void Update(double deltatime);
 	virtual void End();

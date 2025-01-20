bccceb07b44ea3d68a17b2d520618b438ed6f17d
charlie-flynn <charlie.flynn.business@gmail.com>
2025-01-20 14:28:05 -0600

    expanded upon Bullet so that you can see it when it gets shot and has a collider

--- TanksGame/src/Game/Bullet.cpp
+++ TanksGame/src/Game/Bullet.cpp
@@ -1,13 +1,26 @@
 #include "Bullet.h"
 #include "Engine/Actor.h"
 #include "Engine/Transform2D.h"
+#include "Engine/SquareCollider.h"
+#include "raylib.h"
 
 Bullet::Bullet(Vec2 direction)
 {
 	m_direction = direction.getNormalized();
+	AddComponent<SquareCollider>(m_collider = new SquareCollider(new Vec2(10, 10), this));
 }
 
 void Bullet::Update(double deltaTime)
 {
+	// move
 	GetTransform()->Translate(m_direction * 20 * deltaTime);
+
+	// draw
+	Rectangle rectangle = Rectangle();
+	rectangle.x = 10 * GetTransform()->GetGlobalPosition().x;
+	rectangle.y = 10 * GetTransform()->GetGlobalPosition().y;
+	rectangle.width = GetTransform()->GetGlobalScale().x;
+	rectangle.height = GetTransform()->GetGlobalScale().y;
+
+	DrawRectanglePro(rectangle, { 0, 0 }, GetTransform()->GetGlobalRotationAngle() * (180 / PI), RED);
 }
--- TanksGame/src/Game/Bullet.h
+++ TanksGame/src/Game/Bullet.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "Engine/Actor.h"
+#include "Engine/SquareCollider.h"
 
 class Bullet : public Actor
 {
 public:
 	Bullet(Vec2 direction = Vec2(0, 1));
 	void Update(double deltaTime) override;
+	SquareCollider* GetCollider() { return m_collider; };
+
 private:
 	Vec2 m_direction;
+	SquareCollider* m_collider;
 };
